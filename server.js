d8ddcaa3f3b0bb0feaa494e4845e38c685d75e11
leonardsmalls <leonard.smalls37@gmail.com>
2017-07-08 01:49:12 -0500

    qwerty

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ var PORT = process.env.PORT || 8080;
 // Requiring our models for syncing
 var db = require("./models");
 
+var pusher = new Pusher({
+	  appId: '364514',
+	  key: '8fe47ff9e08c85874fce',
+	  secret: 'e263dd3ee43403bae006',
+	  cluster: 'us2',
+	  encrypted: true
+	});
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,6 +33,10 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Static directory
 app.use(express.static("./public"));
+
+pusher.trigger('my-channel', 'my-event', {
+  "message": "hiiiiiiii"
+});
 
 // Routes =============================================================
 
