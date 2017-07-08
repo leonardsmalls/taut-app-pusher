0d70b730d91394ce3169de71c9b5ec07ebfe0779
leonardsmalls <leonard.smalls37@gmail.com>
2017-07-07 23:32:03 -0500

    tautpushpush

--- routes/pusher.js
+++ routes/pusher.js
@@ -1,13 +1,15 @@
 var Pusher = require('pusher');
 
-var pusher = new Pusher({
-  appId: '364514',
-  key: '8fe47ff9e08c85874fce',
-  secret: 'e263dd3ee43403bae006',
-  cluster: 'us2',
-  encrypted: true
-});
+module.exports = function(app) {
+	var pusher = new Pusher({
+	  appId: '364514',
+	  key: '8fe47ff9e08c85874fce',
+	  secret: 'e263dd3ee43403bae006',
+	  cluster: 'us2',
+	  encrypted: true
+	});
 
-pusher.trigger('my-channel', 'my-event', {
-  "message": "goodby cruel hello world"
-});
+	pusher.trigger('my-channel', 'my-event', {
+	  "message": "goodby cruel hello world"
+	});
+}
