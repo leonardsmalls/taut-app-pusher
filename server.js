3cd850ed30b5c5a6c61335f3cdfa679e3175b649
leonardsmalls <leonard.smalls37@gmail.com>
2017-07-07 23:27:13 -0500

    tautpushpush

--- server.js
+++ server.js
@@ -7,20 +7,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var Pusher = require('pusher');
-
-var pusher = new Pusher({
-  appId: '364514',
-  key: '8fe47ff9e08c85874fce',
-  secret: 'e263dd3ee43403bae006',
-  cluster: 'us2',
-  encrypted: true
-});
-
-pusher.trigger('my-channel', 'my-event', {
-  "message": "hello world"
-});
-
 // Sets up the Express App
 // =============================================================
 var app = express();
@@ -42,6 +28,7 @@ app.use(express.static("./public"));
 
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
+require("./routes/pusher.js")(app);
 
 // Syncing our sequelize models and then starting our express app
 db.sequelize.sync({ force: true }).then(function() {
