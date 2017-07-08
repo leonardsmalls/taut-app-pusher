d72845c03c774fffe94aac101ab5cd854aadf625
leonardsmalls <leonard.smalls37@gmail.com>
2017-07-08 04:50:05 -0500

    request test

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -58,7 +58,7 @@ module.exports = function(app) {
   });
 
   // Webhook endpoint
-app.post("/api/client_event", function(req, res) {
+app.post("/api/taut", function(req, res) {
   var timestamp = req.body.time_ms;
   var events = req.body.events;
 
