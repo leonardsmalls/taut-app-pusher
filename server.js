4aff5b432db3f503e828499ca2b72d4a3a32ee7e
leonardsmalls <leonard.smalls37@gmail.com>
2017-07-08 04:20:38 -0500

    request test

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var request = require('request');
-var url ='https://floating-reef-87288.herokuapp.com/'
+var url ='https://requestb.in/1icwwoy1'
 request(url, function (error, response, body) {
   if (!error) {
     console.log(body);
